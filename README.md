3f72db07058753c0d304d8103d3121471928c10d
kiran <kiran.rambha.1995@gmail.com>
2021-01-23 17:31:59 +0000

    Electron App with React and Tailwind Boilerplate

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# electron-react-boilerplate
-1. First install dependencies: ```npm install``` </br>
-2. In one terminal window run: ```npm run watch``` to compile react code <br/>
-3. In other one run: ```npm start``` to start Electron app
+# electron-react-boilerplate with tailwindcss
+
+1. First install dependencies: `npm install` </br>
+2. In one terminal window run: `yarn watch` to compile react code <br/>
+3. In other one run: `yarn start` to start Electron app
--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,19 @@
-
 import React from 'react';
 
-
 export default function App() {
-
   return (
-    <>
-      <h1>I am App Component!!!</h1>
-      <button onClick={() => {
-        electron.notificationApi.sendNotification('My custom notification!');
-      }}>Notify</button>
-    </>
-  )
+    <div>
+      <h1 className='italic text-red-700'>
+        Electron App with React and Tailwind
+      </h1>
+      <button
+        className='mr-5 bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-lg'
+        onClick={() => {
+          electron.notificationApi.sendNotification('My custom notification!');
+        }}
+      >
+        Notification
+      </button>
+    </div>
+  );
 }
